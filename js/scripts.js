f2b1bdff6c12c428406ed676ef20b3e32bd9e2ab
kahinee87 <Aberllister01>
2019-04-25 08:11:13 -0400

    project-4
    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';;
 
 var map = new mapboxgl.Map({
   container: 'mapContainer',
