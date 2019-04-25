d87aeed5845071b8461e5587435801d4ecf8818a
kahinee87 <Aberllister01>
2019-04-24 21:45:36 -0400

    Project-4

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,120 @@ var map = new mapboxgl.Map({
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxxgl.NavigationControl());
-map.on('style.load', function(){
+
+var LandUseLookup = (code) => {
+  switch (code) {
+    case 1:
+      return {
+        color: '#f4f455',
+        description: '1 & 2 Family',
+      };
+    case 2:
+      return {
+        color: '#f7d496',
+        description: 'Multifamily Walk-up',
+      };
+    case 3:
+      return {
+        color: '#FF9900',
+        description: 'Multifamily Elevator',
+      };
+    case 4:
+      return {
+        color: '#f7cabf',
+        description: 'Mixed Res. & Commercial',
+      };
+    case 5:
+      return {
+        color: '#ea6661',
+        description: 'Commercial & Office',
+      };
+    case 6:
+      return {
+        color: '#d36ff4',
+        description: 'Industrial & Manufacturing',
+      };
+    case 7:
+      return {
+        color: '#dac0e8',
+        description: 'Transportation & Utility',
+      };
+    case 8:
+      return {
+        color: '#5CA2D1',
+        description: 'Public Facilities & Institutions',
+      };
+    case 9:
+      return {
+        color: '#8ece7c',
+        description: 'Open Space & Outdoor Recreation',
+      };
+    case 10:
+      return {
+        color: '#bab8b6',
+        description: 'Parking Facilities',
+      };
+    case 11:
+      return {
+        color: '#5f5f60',
+        description: 'Vacant Land',
+      };
+    case 12:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+    default:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+  }
+};
+
+// use jquery to programmatically create a Legend
+// for numbers 1 - 11, get the land use color and description
+for (var i=1; i<12; i++) {
+  // lookup the landuse info for the current iteration
+  const landuseInfo = LandUseLookup(i);
+
+  // this is a simple jQuery template, it will append a div to the legend with the color and description
+  $('.legend').append(`
+    <div>
+      <div class="legend-color-box" style="background-color:${landuseInfo.color};"></div>
+      ${landuseInfo.description}
+    </div>
+  `)
+}
+
+// a little object for looking up neighborhood center points
+var neighborHoodLookup = {
+  'park-slope': [-73.979702, 40.671199],
+  'morningside-heights': [-73.962750, 40.809099],
+  'fidi': [-74.007468, 40.710800],
+  'greenpoint': [-73.951,40.732169],
+}
+
+
+// we can't add our own sources and layers until the base style is finished loading
+map.on('style.load', function() {
+  // add a button click listener that will control the map
+  // we have 4 buttons, but can listen for clicks on any of them with just one listener
+  $('.flyto').on('click', function(e) {
+    // pull out the data attribute for the neighborhood using query
+    var neighborhood = $(e.target).data('neighborhood');
+
+    // this is a useful notation for looking up a key in an object using a variable
+    var center = neighborHoodLookup[neighborhood];
+
+    // fly to the neighborhood's center point
+    map.flyTo({center: center, zoom: 14});
+  });
+
+  // let's hack the basemap style a bit
+  // you can use map.getStyle() in the console to inspect the basemap layers
+  map.setPaintProperty('water', 'fill-color', '#a4bee8')
+
   //this sets up the geojson as the source on themap which we can use to add visuals layers
   map.addSource('greenpoint-pluto',{
     type: 'geojson',
